0f78cdc2406332bb01b58cd3edd5d5a6611fc698
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-01-29 20:42:47 -0600

    Proyecto profesores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BindingComponent } from './binding/binding.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,BindingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
