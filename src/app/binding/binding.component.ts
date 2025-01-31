84b8d78b10424ad715e8b3c6783e7d6504ed244a
Edson-Moreno1 <edson.morenoortega@gmail.com>
2025-01-30 21:15:04 -0600

    Nueva actulizacion

--- src/app/binding/binding.component.ts
+++ src/app/binding/binding.component.ts
@@ -49,4 +49,44 @@ export class BindingComponent {
       "El diseño de Experiencia de Usuario (UX) es el proceso de crear productos, sistemas o servicios que proporcionen experiencias significativas y relevantes a los usuarios. Involucra todo el proceso de adquisición e integración del producto, lo que incluye aspectos de marca, diseño, usabilidad y funcionalidad. Una gran parte del diseño UX es hacer que el producto final resulte funcional para proporcionar una experiencia positiva, eficiente y satisfactoria para el usuario.",
     curse_image: "diseno-ux",
   };
-}
+prof5: any = {
+  name: "Lucas Solis",
+  position: "Programador Frontend",
+  course: "Administración de Redes y Servidores",
+  image: "/Lucas-Solis.jpg",
+  descripcion:
+  "Aprenderás sobre la configuración, mantenimiento y optimización de redes y servidores, abordando protocolos, seguridad y monitoreo de la infraestructura TI.",
+  curse_image: "/redes-servidores",
+};
+prof6: any = {
+  name: "Carlos Gomez",
+  position: "QA Tester",
+  course: "Ciberseguridad y Protección de Datos",
+  image: "/Carlos-Gomez.jpg",
+  descripcion:
+  "Explorarás estrategias para proteger sistemas y datos, analizando amenazas, vulnerabilidades y técnicas de mitigación para garantizar la seguridad de la información.",
+  curse_image: "/ciberseguridad",
+};
+prof7: any = {
+  name: "Cinthia Ortiz",
+  position: "Desarrolladora Web",
+  course: "Gestión de Proyectos Tecnológicos",
+  image: "/Cinthia-Ortiz.jpg",
+  descripcion:
+  "Desarrollarás habilidades en planificación, ejecución y control de proyectos de TI, utilizando metodologías ágiles como Scrum y Kanban para optimizar el desarrollo tecnológico.",
+curse_image: "/gestion-proyectos",
+
+};
+prof8: any = {
+  name: "Rodrigo Diaz",
+  position: "Experto en Big Data",
+  course: "Computación en la Nube",
+  image: "/Rodrigo-Diaz.jpg",
+  descripcion:
+  "Conocerás los principales servicios de computación en la nube (AWS, Azure, Google Cloud), aprendiendo sobre despliegue de aplicaciones, almacenamiento y virtualización.",
+curse_image: "/computacion-nube",  
+};
+
+
+
+} // Fin de la clase BindingComponent
